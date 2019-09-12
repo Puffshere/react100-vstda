77d029b7bcee373b08b1db37d1f2e74861e9f5c3
Shawn Taylor <shawncurtistaylor@gmail.com>
2019-09-12 15:44:44 -0700

    initial vstda push

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,88 @@ class App extends Component {
   render() {
     return (
       <div className='container'>
-
+        <h1 className="text-white">Very Simple Todo App</h1>
+        <h3 className="text-white">Track of all things</h3>
+        <hr className="my-4"></hr>
+        <div className="row">
+          <div className="col">
+            <Addnew />
+          </div>
+          <div className="col">
+            <Todolist />
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+
+class Addnew extends Component {
+
+  render() {
+    return (
+
+      <div className="card">
+        <div className="card-header">
+          Add New Todo
+            </div>
+        <div className="card-body">
+          <p className="font-weight-bold">I want to..</p>
+          <textarea className="create-todo-text">
+          </textarea>
+          <p className="font-weight-bold">How much of a priority is this?</p>
+          <div className="dropdown">
+            <select className="create-todoo-priority btn-block">
+              <option>Select a priority</option>
+              <option value="1"></option>
+              <option value="2"></option>
+              <option value="3"></option>
+            </select>
+          </div>
+        </div>
+        <div className="card-footer">
+          <button type="btn" className="btn btn-success btn-block">Add</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class Todolist extends Component {
+
+  render() {
+    return (
+
+      <div className="card">
+        <div className="card-header">
+          View Todos
+        </div>
+
+
+        <div className="card-body-1 alert-warning">
+          <input type="checkbox"></input>
+
+          <a className="edit-todo">
+          </a>
+
+          <div className="card-body-2 alert-danger">
+            <input type="checkbox"></input>
+            <div className="card-body-3 alert-success">
+              <input type="checkbox"></input>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    );
+  }
+}
+
 
 export default App;
