beacabcbea0985f96f97a81a5b2c5dc588ce354f
Shawn Taylor <shawncurtistaylor@gmail.com>
2019-09-13 13:17:27 -0700

    3rd push

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Todolist from './Todolist';
+import Addnew from './Addnew';
 
 class App extends Component {
 
@@ -10,10 +13,10 @@ class App extends Component {
         <hr className="my-4"></hr>
         <div className="row">
           <div className="col">
-            <Addnew />
+            <Addnew/>
           </div>
           <div className="col">
-            <Todolist />
+            <Todolist/>
           </div>
         </div>
       </div>
@@ -22,64 +25,9 @@ class App extends Component {
 }
 
 
-class Addnew extends Component {
-
-  render() {
-    return (
-
-      <div className="card">
-        <div className="card-header">
-          Add New Todo
-            </div>
-        <div className="card-body">
-          <p className="font-weight-bold">I want to..</p>
-          <textarea className="create-todo-text">
-          </textarea>
-          <p className="font-weight-bold">How much of a priority is this?</p>
-          <div className="dropdown">
-            <select className="create-todoo-priority btn-block">
-              <option>Select a priority</option>
-              <option value="1"></option>
-              <option value="2"></option>
-              <option value="3"></option>
-            </select>
-          </div>
-        </div>
-        <div className="card-footer">
-          <button type="btn" className="btn btn-success btn-block">Add</button>
-        </div>
-      </div>
-    );
-  }
-}
 
 
-class Todolist extends Component {
 
-  render() {
-    return (
-
-      <div className="card">
-        <div className="card-header">
-          View Todos
-        </div>
-
-
-        <div className="card-body-1 alert-primary">
-          <h5 className="font-weight-bold">Welcome to Very Simple Todo App!</h5>
-          <h5>Get started now by adding a new todo on the left.</h5>
-
-        
-
-            </div>
-
-          </div>
-
-        
-
-    );
-  }
-}
 
 
 export default App;
---
+++ src/Todolist.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+ 
+ 
+ 
+ export default class Todolist extends Component {
+
+    render() {
+      return (
+  
+        <div className="card">
+          <div className="card-header">
+            View Todos
+          </div>
+  
+          <div className="card-body alert-primary">
+            <strong>Welcome to Very Simple Todo App!</strong>
+            <p>Get started now by adding a new todo on the left.</p>
+          </div>
+  
+        </div>
+  
+  
+  
+      );
+    }
+  }
